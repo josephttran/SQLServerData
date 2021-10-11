68b6ea2bb11d8591696c3cb760b7be8c43dc37f2
josephttran <42635012+josephttran@users.noreply.github.com>
2021-10-10 17:45:26 -0700

    Display quarterly sales

---
+++ SQLServerData/View/SaleByQuarter.sql
@@ -0,0 +1,20 @@
+﻿CREATE VIEW [dbo].[SaleByQuarter]
+	AS 
+	SELECT 
+		SUM(CASE 
+				WHEN Month = 'January' OR Month = 'February' OR Month = 'March' THEN Amount
+			END	
+			) AS Quarter1,
+		SUM(CASE 
+				WHEN Month = 'April' OR Month = 'May' OR Month = 'June' THEN Amount
+			END	
+			) AS Quarter2,
+		SUM(CASE 
+				WHEN Month = 'July' OR Month = 'August' OR Month = 'September' THEN Amount
+			END	
+			) AS Quarter3,
+		SUM(CASE 
+				WHEN Month = 'October' OR Month = 'November' OR Month = 'December' THEN Amount
+			END	
+			) AS Quarter4
+	FROM [dbo].[Sale]
