f455957ee78858ac840bf79999d388a799ff3839
josephttran <42635012+josephttran@users.noreply.github.com>
2021-10-10 17:22:55 -0700

    Display months from row to column with sum amount for each month

---
+++ SQLServerData/Stored Procedures/usp_SalePivotMonthSumAmount.sql
@@ -0,0 +1,36 @@
+﻿CREATE PROCEDURE [dbo].[usp_SalePivotMonthSumAmount]
+AS
+	SELECT	January,
+			February,
+			March,
+			April,
+			May,
+			June,
+			July,
+			August,
+			September,
+			October,
+			November,
+			December
+	FROM 
+	(
+		SELECT [Month], [Amount]
+		FROM [dbo].[Sale]
+	) AS S
+	PIVOT
+	(
+		SUM(Amount)
+		FOR Month IN ("January",
+					"February",
+					"March",
+					"April",
+					"May",
+					"June",
+					"July",
+					"August",
+					"September",
+					"October",
+					"November",
+					"December")
+	) AS P
+RETURN 0
